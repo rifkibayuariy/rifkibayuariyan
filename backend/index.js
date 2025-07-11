eee963b721157ad7fc15bb57c9fd86ce02b9a636
rifkibayuariy <email.rifkibayu@gmail.com>
2025-07-11 16:25:19 +0700

    before connect vercel

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const cors = require("cors");
-const { educationHistory, skills, projects } = require("./data");
+const { sql } = require("@vercel/postgres");
 const app = express();
-const PORT = 3001;
+
 app.use(cors());
 
-app.use(express.json());
-app.get("/api/education", (req, res) => res.json(educationHistory));
-app.get("/api/skills", (req, res) => res.json(skills));
-app.get("/api/projects", (req, res) => res.json(projects));
-app.listen(PORT, () => {
-  console.log(` Server backend berjalan di
-http://localhost:${PORT}`);
+app.get("/api/education", async (req, res) => {
+  try {
+    const { rows } = await sql`SELECT * FROM education ORDER BY period
+DESC;`;
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ error: "Gagal mengambil data pendidikan" });
+  }
 });
+app.get("/api/skills", async (req, res) => {
+  try {
+    const { rows } = await sql`SELECT * FROM skills;`;
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ error: "Gagal mengambil data skill" });
+  }
+});
+app.get("/api/projects", async (req, res) => {
+  try {
+    const { rows } = await sql`SELECT * FROM projects;`;
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ error: "Gagal mengambil data proyek" });
+  }
+});
+module.exports = app;
